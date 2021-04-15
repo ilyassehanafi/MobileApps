7497ae2728ddc585a388f8bd7422180803159ba0
Ilyasse HANAFI <ilyassehanafi@gmail.com>
2021-04-15 17:52:51 +0000

    picture,radio buttons added to the app

---
+++ TP_01/app/src/main/java/com/example/etab_univ/EtabUniv.java
@@ -0,0 +1,23 @@
+package com.example.etab_univ;
+
+public class EtabUniv {
+
+    private String name   = "";
+    private String adress = "";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+}
--- TP_01/app/src/main/java/com/example/etab_univ/MainActivity.java
+++ TP_01/app/src/main/java/com/example/etab_univ/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.etab_univ;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EtabUniv etabUniv = new EtabUniv();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText name = findViewById(R.id.name);
+        EditText adresse = findViewById(R.id.addr);
+        Button save = findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText name    = findViewById(R.id.name);
+                EditText adresse = findViewById(R.id.addr);
+                etabUniv.setName(name.getText().toString());
+                etabUniv.setAdress(adresse.getText().toString());
+                Toast.makeText(MainActivity.this, "le nom est saisi", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "l'adresse est saisi", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
